8fc059f060ef2a60de214766a7966dc6ec2bb2d6
tmabraham <tmabraham@ucdavis.edu>
2018-03-09 12:26:23 -0800

    Update VJ+KLT for real-time

--- video_analysis/webcam_capture.py
+++ video_analysis/webcam_capture.py
@@ -10,7 +10,7 @@ import numpy as np
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
-video = cv2.VideoCapture("C:/Users/Bijta/Documents/GitHub/non-contact-heart-rate/video_analysis/test/VJ+KLT_test.mp4") 
+video = cv2.VideoCapture(0) 
 
 # Check if camera opened successfully
 if (video.isOpened()== False): 
