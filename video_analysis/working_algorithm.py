cce2304760e64f34685eb1cddb34de8929608cf5
unknown <tmabraham@ucdavis.edu>
2018-05-11 00:32:45 -0700

    added pyfftw

--- video_analysis/working_algorithm.py
+++ video_analysis/working_algorithm.py
@@ -12,7 +12,8 @@ import cv2
 import datetime
 import numpy as np
 from scipy import signal
-from scipy.fftpack import fft, fftfreq, fftshift
+from scipy.fftpack import fftfreq, fftshift
+import pyfftw
 from sklearn.decomposition import PCA, FastICA
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
@@ -55,6 +56,9 @@ def skin_detection(frame):
     # Do contour detection on skin region
     im, contours, hierarchy = cv2.findContours(skinRegion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return im
+
+pyfftw.interfaces.cache.enable()
+pyfftw.interfaces.cache.set_keepalive_time(1.0)
 # open video file
 #cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture("C:\\Users\\Bijta\\Documents\\GitHub\\non-contact-heart-rate\\video_analysis\\test\\HR2.mov")
@@ -72,7 +76,7 @@ while cap.isOpened():
             time.append(0)
             # Take first frame and find face in it
             firstFrame = frame
-            cv2.imshow("frame",firstFrame)
+            #cv2.imshow("frame",firstFrame)
             old_gray = cv2.cvtColor(firstFrame, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(old_gray, 1.3, 5) # Use Viola-Jones classifier to detect face
             if faces == ():
@@ -82,13 +86,13 @@ while cap.isOpened():
                     x2 = x+w # other side of rectangle, x
                     y2 = y+h # other side of rectangle, y
                     cv2.rectangle(firstFrame,(x,y),(x+w,y+h),(255,0,0),2) #draw rect.
-                    cv2.imshow("frame",firstFrame)
+                    #cv2.imshow("frame",firstFrame)
                     VJ_mask = np.zeros_like(firstFrame)
                     VJ_mask = cv2.rectangle(VJ_mask,(x,y),(x+w,y+h),(255,0,0),-1)
                     VJ_mask = cv2.cvtColor(VJ_mask, cv2.COLOR_BGR2GRAY)
                 ROI = cv2.bitwise_and(VJ_mask,im)
                 ROI_color = cv2.bitwise_and(ROI,ROI,mask=VJ_mask)
-                cv2.imshow('ROI',ROI_color)
+                #cv2.imshow('ROI',ROI_color)
                 
                 #take average signal in the region of interest (mask)
                 _,G_new,_,_ = cv2.mean(ROI_color,mask=ROI) 
@@ -120,7 +124,7 @@ while cap.isOpened():
                 transformed = np.zeros_like(np.array([[[x,y]],[[x2,y]],[[x2,y2]],[[x,y2]]]))
                 tmatrix = cv2.estimateRigidTransform(good_old,good_new,fullAffine=True)
                 transformed = cv2.transform(np.array([[[x,y]],[[x2,y]],[[x2,y2]],[[x,y2]]]),tmatrix)
-                cv2.imshow('frame',frame)
+                #cv2.imshow('frame',frame)
                 x = transformed[0,0,0]
                 y = transformed[0,0,1]
                 x2 = transformed[2,0,0]
@@ -131,7 +135,7 @@ while cap.isOpened():
                 VJ_mask = cv2.rectangle(VJ_mask,(x,y),(x2,y2),(255,255,255),-1)
                 ROI = cv2.bitwise_and(VJ_mask,im)
                 ROI_color = cv2.bitwise_and(frame,frame,mask=ROI)
-                cv2.imshow('ROI',ROI_color)
+                #cv2.imshow('ROI',ROI_color)
                 #take average signal in the region of interest (mask)
                 _,G_new,_,_ = cv2.mean(ROI_color, mask=ROI)
                 G.append(G_new)
@@ -154,13 +158,13 @@ while cap.isOpened():
                     
                     #FFT
                     N = len(X_f)
-                    yf = fft(X_f)
+                    yf = pyfftw.interfaces.scipy_fftpack.fft(X_f)
                     yf = yf/np.sqrt(N) #Normalize FFT
                     xf = fftfreq(N, T) # FFT frequencies 
                     xf = fftshift(xf) #FFT shift
                     yplot = fftshift(abs(yf))
-                    plt.figure(1)
-                    plt.gcf().clear()
+                    #plt.figure(1)
+                    #plt.gcf().clear()
                     fft_plot = yplot
                     # Find highest peak between 0.75 and 4 Hz 
                  #   fft_plot[xf<=0.75] = 0 
@@ -169,7 +173,7 @@ while cap.isOpened():
                     else:
                         bpm = 0.9*bpm + 0.1*(xf[(xf>=0.75) & (xf<=4)][fft_plot[(xf>=0.75) & (xf<=4)].argmax()]*60)
                     print(str(bpm)+' bpm') # Print heart rate
-                    plt.plot(xf[(xf>=0)], fft_plot[(xf>=0)]) # Plot FFT
+                    #plt.plot(xf[(xf>=0)], fft_plot[(xf>=0)]) # Plot FFT
                     plt.pause(0.001)
             if frame_num % 10 == 0:
                 print(frame_num)
